ebb259d03822cdb8ab7a7a8dbde82a3dea066bb0
Sagar <sagar@tagalys.com>
2025-02-12 17:03:07 +0530

    Sidekiq implementation

---
+++ app/sidekiq/test_job.rb
@@ -0,0 +1,8 @@
+class TestJob
+  include Sidekiq::Job
+
+  def perform(name, age)
+    puts "I am #{name}, running my first job at #{age}"
+    # any other valid Ruby/Rails code goes here!
+  end
+end
